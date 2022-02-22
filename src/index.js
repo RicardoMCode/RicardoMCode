6ceadae03dc5a7b683fc40518832e5ac08de78b2
RicardoMaldonadoV <ricardomaldonado1204@gmail.com>
2022-02-22 00:56:46 -0500

    [UPDATE] aplicación de media queries para responsive desing, mostly fluid, se unifica el consumo del css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Import the routes 
 import App from './routes/App';
-//Import global css
-import './styles/global.css'
 ReactDOM.render(<App />, document.getElementById('app'));
 
